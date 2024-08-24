99c9de1fb77dcb54c8d49959257a6d75aa503554
= <info@farunurisonmez.com>
2024-08-24 15:32:16 +0300

    #2
    Co-Authored-By: Faru Nuri Sönmez <info@farunurisonmez.com>

--- src/components/navbars/app.navbar.comp.tsx
+++ src/components/navbars/app.navbar.comp.tsx
@@ -4,11 +4,11 @@ import { CgFileDocument } from "react-icons/cg";
 
 const AppNavbarComp = () => {
     return (
-        <nav className="fixed top-0 w-full z-10 mt-0 md:mt-10">
-            <div className="container w-[90%] mx-auto px-4 md:px-6 py-4 flex justify-between items-center bg-[#2354e6] shadow-lg rounded-b-lg md:rounded-2xl">
+        <nav className="fixed top-0 w-full z-10 mt-0 md:mt-0">
+            <div className="container w-[90%] mx-auto px-4 md:px-6 py-4 flex justify-between items-center bg-[#2354e6] shadow-lg rounded-b-3xl md:rounded-b-3xl">
                 {/* Logo ve Title */}
                 <div className="flex items-center space-x-2 md:space-x-4">
-                    <img src="https://raw.githubusercontent.com/DevWikz/wikz-web-app/dev/v1.0.1/public/images/wikz-circle-dark-logo.png" alt="Logo" className="w-8 h-8 md:w-10 md:h-10" />
+                    <img src="https://raw.githubusercontent.com/DevWikz/wikz-web-app/dev/v1.0.1/public/images/wikz_logo.svg" alt="Logo" className="w-8 h-8 md:w-10 md:h-10" />
                     <span className="text-3xl md:text-5xl  text-white font-bold">WikZ</span>
                 </div>
 
--- src/pages/HomePage/Home.page.tsx
+++ src/pages/HomePage/Home.page.tsx
@@ -4,14 +4,19 @@ const HomePage: React.FC = () => {
     return (
         <>
             <div className='min-h-screen flex items-center'>
-                <div className="container mx-auto px-6 flex flex-col md:flex-row mt-32 md:mt-36">
+                <div className="container mx-auto px-6 flex flex-col md:flex-row mt-32 md:mt-16">
                     {/* Sol Taraf */}
                     <div className="w-full md:w-2/3 flex flex-col justify-center space-y-8">
-                        <h1 className="text-3xl md:text-5xl font-extrabold text-white text-center md:text-start">
-                            Are You Ready for Telegram's <br />
+                        <h1 className="leading-[40px] md:leading-[64px] text-3xl md:text-[3rem] font-extrabold text-white text-center md:text-start ">
+                            <p>
+                            Are You Ready for Telegram's 
+                            </p>
+                            <p>
                             Ultimate Online Store?
+                            </p>
+                         
                         </h1>
-                        <div className="bg-[#111e55] text-white p-3 md:p-10 rounded-3xl shadow-lg text-center md:text-start">
+                        <div className="bg-[#111e55] text-white p-5 md:p-10 rounded-3xl shadow-lg text-center md:text-start">
                             <p className="text-sm md:text-xl font-extralight">
                                 Imagine snagging a piece of the <strong className="font-bold">next big thing</strong>—like getting in on Amazon
                                 in its early days, but this time, it's free! Just launch the <strong className="font-bold">WikZ app</strong>, spin the
@@ -23,7 +28,7 @@ const HomePage: React.FC = () => {
                             </p>
 
                             {/* Start WikZ Button */}
-                            <div className="flex justify-center mt-10">
+                            <div className="flex justify-center mt-6">
                                 <button className="bg-[#5371ff] text-white font-extrabold text-sm md:text-2xl px-6 rounded-lg shadow-lg w-32 h-8 md:w-52 md:h-12">
                                     Start WikZ
                                 </button>
@@ -42,28 +47,28 @@ const HomePage: React.FC = () => {
                 {/* WikZ Swag Section */}
                 <div className="text-center mb-12">
                     <h1 className="text-3xl md:text-5xl font-bold text-white py-3 md:py-6">WikZ Swag</h1>
-                    <h2 className="text-1xl md:text-3xl font-bold mt-0 md:mt-2" style={{ color: '#5371ff' }}>
+                    <h2 className="text-[20px] md:text-3xl font-bold mt-0 md:mt-1" style={{ color: '#5371ff' }}>
                         Your Hub for Exclusive Web3 Swag
                     </h2>
-                    <div className="text-center mt-0 md:mt-12">
-                        <p className="text-lg md:text-xl font-bold text-white">On the journey to becoming the premier swag store for the Web3 community</p>
+                    <div className="text-center mt-0 md:mt-0">
+                        <p className="text-[16px] md:text-xl font-bold text-white">On the journey to becoming the premier swag store for the Web3 community</p>
                     </div>
                 </div>
 
                 <div className="flex flex-col lg:flex-row justify-between items-start">
                     {/* Right Section with Image (now on the left) */}
-                    <div className="w-full lg:w-2/4 flex justify-center items-center lg:mr-4 mt-0 mb-12 md:md-0 md:mt-12  lg:mt-0">
+                    <div className="w-full lg:w-[550px] flex justify-center items-center lg:mr-4 mt-0 mb-12 md:md-0 md:mt-12  lg:mt-0">
                         <img src="https://raw.githubusercontent.com/DevWikz/wikz-web-app/dev/v1.0.1/public/images/swag-img.png" alt="Mobile App" className="w-full h-auto" />
                     </div>
 
                     {/* Left Section with Cards (now on the right) */}
-                    <div className="w-full space-y-8">
+                    <div className="w-full lg:w-1/2 space-y-8">
                         <div className="grid grid-cols-2 gap-4 sm:grid-cols-2">
                             {/* 1.Kart */}
                             <div className="bg-[#1a2024] text-white p-2 rounded-[1rem] shadow-lg flex justify-center items-center">
                                 <div className="text-center">
                                     <h3 className="text-sm md:text-xl font-bold mb-2" style={{ color: '#5371ff' }}>Shop Unique Swag</h3>
-                                    <p className='text-[10px] md:text-[15px]'>Exclusive items directly from top crypto brands' stores,<br /> hosted on WikZ Swag.</p>
+                                    <p className='text-[10px] md:text-[15px]'>Exclusive items directly from top crypto brands' stores, hosted on WikZ Swag.</p>
                                 </div>
                             </div>
                             {/* 2.Kart */}
@@ -84,13 +89,13 @@ const HomePage: React.FC = () => {
                             <div className="bg-[#1a2024] text-white p-2 rounded-[1rem] shadow-lg flex justify-center items-center">
                                 <div className="text-center">
                                     <h3 className="text-sm md:text-xl font-bold mb-2" style={{ color: '#5371ff' }}>Flexible Payments</h3>
-                                    <p className='text-[10px] md:text-[15px]'>Pay easily with major<br /> cryptocurrencies or traditional <br /> methods.</p>
+                                    <p className='text-[10px] md:text-[15px]'>Pay easily with major<br /> cryptocurrencies or traditional methods.</p>
                                 </div>
                             </div>
                         </div>
 
                         {/* Personalized Shopping Section */}
-                        <div className="w-full mt-8">
+                        <div className="w-full mt-4">
                             <div className="bg-[#1a2024] text-white p-4 rounded-[1rem] shadow-lg justify-center items-center">
                                 <div className="text-center">
                                     <h3 className="text-sm md:text-xl font-bold mb-2" style={{ color: '#5371ff' }}>Personalized Shopping</h3>
@@ -107,11 +112,11 @@ const HomePage: React.FC = () => {
             <div className="px-5 py-5 md:px-24 md:py-12">
                 <div className="text-center mb-12">
                     <h1 className="text-3xl md:text-5xl font-bold text-white py-3 md:py-6">WikZ Digital</h1>
-                    <h2 className="text-1xl md:text-3xl font-bold mt-0 md:mt-2" style={{ color: '#5371ff' }}>
+                    <h2 className="text-[20px] md:text-3xl font-bold mt-0 md:mt-2" style={{ color: '#5371ff' }}>
                         WikZ Digital brings key services to Telegram app
                     </h2>
-                    <div className="text-center mt-0 md:mt-12">
-                        <p className="text-lg md:text-xl font-bold text-white">WikZ Digital simplifies your digital life, keeping everything you need right at your fingertips within Telegram</p>
+                    <div className="text-center mt-0 md:mt-0">
+                        <p className="text-[16px] md:text-xl font-bold text-white">WikZ Digital simplifies your digital life, keeping everything you need right at your fingertips within Telegram</p>
                     </div>
                 </div>
 
@@ -133,7 +138,7 @@ const HomePage: React.FC = () => {
                                 <p className='text-[10px] md:text-[15px]'>Pay bills securely<br /> using crypto.</p>
                             </div>
                             <div className="flex flex-col justify-center items-center bg-[#1a2024] text-white p-2 md:p-4 rounded-[1rem] shadow-lg text-center">
-                                <h3 className="text-xs md:text-xl font-bold mb-2" style={{ color: '#5371ff' }}>E-SIM Solutions</h3>
+                                <h3 className="text-xs md:text-xl font-bold mb-2" style={{ color: '#5371ff' }}>E-SIM</h3>
                                 <p className='text-[10px] md:text-[15px]'>Activate and<br /> manage E-SIMs with ease.</p>
                             </div>
                         </div>
